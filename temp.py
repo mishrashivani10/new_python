163bc0e477372d934306331f9f6d656b21a04969
Avantika <Shivani Mishra>
2019-08-18 10:07:53 +0530

    convert temperature

---
+++ temp.py
@@ -0,0 +1,29 @@
+#exercise 2
+#name=input("enter your name");
+#print (name);
+
+#exercise 3
+#hours= input("enter hours");
+#hours =int(hours);
+#rate= input("enter rate per hour");
+#rate=float(rate);
+#pay= hours*rate;
+#pay=float(pay);
+#print (pay);  
+
+# exercise 4
+#width=17
+#height=12.0
+#print(width//2)
+#print(width/2.0)
+#print(height/3)
+#print(1+2*5)
+
+# exercise 5
+c=input("enter the temperature in celsius")
+c=int(c)
+f=(c+32)*(9/5)
+print (int(f))
+
+
+
